ee507d68df8e237d54c6cb2d13f1348f98cc7d8c
Luis Octavio Antelo <desarrollo.sofiaa2@gmail.com>
2022-04-02 09:14:54 -0400

    Initial commit

---
+++ Engine/Entities/Triangle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Initial_project.Engine.Entities
+{
+    class Triangle
+    {
+
+        //atributos
+        float[] vertices = new float[]
+        {
+            //posiciones
+           -0.4f, -0.0f,  0.0f, // 0
+           -0.4f, -0.4f,  0.0f, // 1 
+            0.0f, -0.4f,  0.0f, // 2
+            0.6f, -0.4f,  0.0f, // 3
+            0.6f,  0.0f,  0.0f, // 4
+            0.4f,  0.2f,  0.0f, // 5 
+           -0.2f,  0.2f,  0.0f, // 6
+            0.0f,  0.0f,  0.0f // 7
+
+        };
+
+        uint[] indices = new uint[]
+        {
+            //líneas
+            0, 1,   1, 2,   2, 3,
+            3, 4,   4, 5,   4, 7,
+            7, 2,   5, 6,   6, 7,
+            6, 0,   7, 0
+        };
+
+
+
+        //constructor
+        public Triangle()
+        {
+        }
+
+        //getters
+        public float[] getVertices()
+        {
+            return vertices;
+        }
+
+        public int verticesCount()
+        {
+            return vertices.Length;
+        }
+
+        public uint[] getIndices()
+        {
+            return indices;
+        }
+
+        public int indexCount()
+        {
+            return indices.Length;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Initial_project.Engine;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace Initial_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                Size = new Vector2i(800, 600),
+                Title = "LearnOpenTK - Creating a Window",
+                // This is needed to run on macos
+                Flags = ContextFlags.ForwardCompatible,
+            };
+
+            using (var window = new Window(GameWindowSettings.Default, nativeWindowSettings))
+            {
+                window.Run();
+            }
+        }
+    }
+}
